973ca105bc262edb72347755ecbb75da9f3e82ae
David Lindblom <daavidlindblom@gmail.com>
2021-11-29 09:17:22 +0100

    changing of structure

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id}) => {
   return (
   
 <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-<img alt ="photo" src ={`https://robohash.org/${id}?200x200`} />
+<img src ={`https://robohash.org/${id}?200x200`} alt ="a robot face" />
 <div>
 <h2>{name}</h2>
 <p>{email}</p>
